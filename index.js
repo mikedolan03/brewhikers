3c5dba81af668a49447c7a596095ba1e2d563c41
Mike Dolan <mikedolan03@gmail.com>
2018-02-24 10:39:22 -0500

    added click listener for go button

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 function main(){
 
-$(".js-pick-hike").click( event => {
-	event.preventDefault();
-	console.log('clicked');
-	$('body').scrollTop( $('.map-view').offset().top);
-} );
+	$(".js-pick-hike").click( event => {
+		event.preventDefault();
+		console.log('clicked');
+		$('body').scrollTop( $('.map-view').offset().top);
+	} );
 
+
+	function renderLocationView(){
+
+	}
+
+	function renderHikeView(){
+
+	}
+
+	function renderBreweryView(){
+
+	}
+
+
+$(".js-pick-location-button").click( event => {
+		event.preventDefault();
+		console.log('clicked');
+		let userLocationChoice = $("select").val(); 
+		console.log("location"+userLocationChoice);
+	} );
 
 }
+
+
+
 
 //on page load
 $(function() {
