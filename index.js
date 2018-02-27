519c90eda4d132ffaee342682842baae2eb99761
Mike Dolan <mikedolan03@gmail.com>
2018-02-26 22:38:52 -0500

    brewery info added, page flow with buttons, aria

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ let userLocationChoice = "Charlottesville";
 let userLat = 0;
 let userLong =0;
 let radiusAmount = 10000;
-
+let hikeData = {};
+let breweryData = {};
+let map;
+let userLoc;
+const userHikes = []; 
+const userBreweries = []; 
+let brewerylistContent = "";
 //getDataFromApi("", renderHikeView);
 
 	$(".js-pick-hike").click( event => {
@@ -20,8 +26,11 @@ let radiusAmount = 10000;
 
 	function renderHikeView(data, status){
 
+		hikeData = data; 
+
 		console.log("getting data: ", data);
 		console.log("getting data: "+ data.trails[0].name);
+		console.log("saved data: ", hikeData);
 
 
 		let listContent  = '';
@@ -34,6 +43,10 @@ let radiusAmount = 10000;
 								</li>`;
 		}
 
+		$('.js-location-view').addClass('hide');
+		$('.js-hike-view').removeClass('hide');
+
+		$('.js-hike-search-results').html(`Choose from ${data.trails.length} hikes listed below.`);
 
 		$('#js-hike-list').html(listContent);
 
@@ -43,8 +56,7 @@ let radiusAmount = 10000;
 			let userHikeChoice =  parseInt( $(event.currentTarget).attr("data") );
 			console.log("hike #"+userHikeChoice);
 
-			$('.js-hike-view').addClass('hide');
-			$('.js-brewery-view').removeClass('hide');
+			userHikes[0] = userHikeChoice; 
 
 			console.log("location"+userLocationChoice);
 
@@ -82,25 +94,123 @@ let radiusAmount = 10000;
 			return;
 		}
 
+		breweryData = data;
+
 		console.log("getting data: ", data);
+				console.log("saved data: ", breweryData);
+
 
 		let listContent = '';
 
-		for(let i = 0; i < data.length; i++) {
+		let breweryCount = data.length;
 
-			listContent += `<li>
-							<img src="http://badgerheadgames.com/wp-content/uploads/2018/02/beer-2370783_1920-e1519612752761-1.jpg" alt="${data[i].name}"><input type="checkbox" name="brewery" id="brewery${i}" value="${i}" data="${i}"><label for="brewery${i}">Add to list</label>
+		if(breweryCount > 6) breweryCount = 6; 
+
+
+
+		for(let i = 0; i < breweryCount; i++) {
+
+			getBreweryDetailDataFromGoogleApi(data[i].place_id, parseBreweryDetails );
+
+				console.log(i , breweryCount);
+
+				brewerylistContent += `<li id='${data[i].place_id}'>
+										<img src="http://badgerheadgames.com/wp-content/uploads/2018/02/beer-2370783_1920-e1519612752761-1.jpg" alt="${data[i].name}">
+										<button name="brewery" id="brewery${data[i].place_id}" data="${data[i].place_id}" class="js-add-brewery-button"> Add to list</button>
+										<p class="brewery-name">${data[i].name}</p>
+										<p class="brewery-summary">${data[i].vicinity}</p>
+										</li>`;
+
+
+
+			}
+
+			/* listContent += `<li>
+							<img src="http://badgerheadgames.com/wp-content/uploads/2018/02/beer-2370783_1920-e1519612752761-1.jpg" alt="${data[i].name}">
+							
+							<input type="checkbox" name="brewery" id="brewery${i}" value="${i}" data="${i}"><label for="brewery${i}">Add to list</label>
 							<p class="brewery-name">${data[i].name}</p>
 							<p class="brewery-summary">${data[i].vicinity}</p>
 							</li>`;
-			}
+			} */
 
-		$('#js-brewery-list').html(listContent);	
+			console.log(brewerylistContent);
+
+		$('.js-hike-view').addClass('hide');
+		$('.js-brewery-view').removeClass('hide');
+
+		$('.js-breweries-search-results').html(`Choose from ${breweryCount} breweries listed below.`);
+
+		$('#js-brewery-list').html(brewerylistContent);	
 
 		//reset to default value just in case we enlarged the search
 		radiusAmount = 10000; 
 
+		
 
+		//------------------create dynamic event listeners for new HIKE BUTTONS 
+
+		 $("#js-brewery-list").on("click", ".js-add-brewery-button", function (event) {
+      		event.preventDefault();
+			
+			userBreweries.push($(event.currentTarget).attr("data"));
+			console.log('clicked', userBreweries);
+
+			$("#js-breweries-selected-go-button").removeClass("greyed");
+
+
+
+				//here we want to then replace the Add button with a Remove button and set a listener to the button.
+				//replace button jquery replace? with button and then add new event listener. 
+
+		});
+
+		$("#js-breweries-selected-go-button").click( event => {
+			event.preventDefault();
+			console.log('clicked');
+			$('.js-brewery-view').addClass('hide');
+			$('#show-map').removeClass('hide');
+			initMap();
+
+		});
+
+
+
+	}
+
+	function parseBreweryDetails(place, status)
+	{
+		let newbrewerylistContent = ""; 
+
+		console.log(status, place);
+
+		newbrewerylistContent += `<li>
+						<img src="http://badgerheadgames.com/wp-content/uploads/2018/02/beer-2370783_1920-e1519612752761-1.jpg" alt="${place.name}">
+						<p class="brewery-name">${place.name}</p>
+						<p class="brewery-address">${place.vicinity}</p>
+						<p class="brewery-rating">${place.rating}</p>
+						<button name="brewery" id="brewery${place.place_id}" data="${place.place_id}" class="js-add-brewery-button"> Add to list</button>`;
+
+		if(place.reviews){
+			newbrewerylistContent += `<p class="brewery-review">Recent Review: ${place.reviews[0].text}</p>`;
+		}
+							
+		newbrewerylistContent += `</li>`;
+
+		$(`#${place.place_id}`).html(newbrewerylistContent);
+
+		/*$(".js-add-brewery-button").click( event => {
+			event.preventDefault();
+			console.log('clicked');
+			userHikes.push(parseInt( $(event.currentTarget).attr("data")));
+
+				//here we want to then replace the Add button with a Remove button and set a listener to the button.
+				//replace button jquery replace? with button and then add new event listener. 
+
+		});
+		*/
+
+				
 
 	}
 
@@ -111,6 +221,23 @@ let radiusAmount = 10000;
 	function parseHikeData(){
 
 	}
+
+	function initMap() {
+        let myLatLng = {lat: userLat, lng: userLong};
+
+        // Create a map object and specify the DOM element for display.
+        let bhmap = new google.maps.Map(document.getElementById('final-map'), {
+          center: myLatLng,
+          zoom: 4
+        });
+
+        // Create a marker and set its position.
+        let marker = new google.maps.Marker({
+          map: bhmap,
+          position: myLatLng,
+          title: 'Hello World!'
+        });
+      }
 
 
 
@@ -142,9 +269,9 @@ function getDataFromBreweryApi(city, callback) {
 function getDataFromBreweryGoogleApi(radius, callback) {
 
 	
- let userLoc = new google.maps.LatLng(userLat,userLong);
+ userLoc = new google.maps.LatLng(userLat,userLong);
 
- let map = new google.maps.Map(document.getElementById('map'), {
+ map = new google.maps.Map(document.getElementById('map'), {
       center: userLoc,
       zoom: 15
     });
@@ -159,6 +286,23 @@ function getDataFromBreweryGoogleApi(radius, callback) {
 
   service = new google.maps.places.PlacesService(map);
   service.nearbySearch(request, callback);
+
+	//$.getJSON(BREWERY_SEARCH_URL, '', callback);
+}
+
+
+function getBreweryDetailDataFromGoogleApi(placeID, callback) {
+
+	console.log(placeID);
+
+ let request = {
+  	placeId: `${placeID}`
+	};
+
+	console.log(request);
+
+service = new google.maps.places.PlacesService(map);
+service.getDetails(request, callback);
 
 	//$.getJSON(BREWERY_SEARCH_URL, '', callback);
 }
@@ -181,8 +325,7 @@ $(".js-pick-location-button").click( event => {
 		userLocationChoice = $("select").val(); 
 		console.log("location"+userLocationChoice);
 
-		$('.js-location-view').addClass('hide');
-		$('.js-hike-view').removeClass('hide');
+		
 
 		if(userLocationChoice == "Great Falls") { 
 			userLat = 38.9982;
